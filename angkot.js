dac457f5f3f217c3e210112785504def2014abf2
achmadnabilfikri <achmadnabil1999@gmail.com>
2024-12-25 15:01:28 +0700

    first commit

---
+++ angkot.js
@@ -0,0 +1,13 @@
+let nmro = 1;
+let nmrt = 6;
+let jmlh = 10;
+let beroperasi = 6;
+
+while (nmro <= beroperasi) {
+  console.log("angkot " + nmro + " beroperasi dengan baik");
+  nmro++;
+}
+
+for (nmrt; nmrt <= jmlh; nmrt++) {
+  console.log("Angkot No." + nmrt + " tidak sedang beroperasi");
+}
